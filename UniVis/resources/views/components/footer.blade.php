a3d13f46382dffd01836994dcd410da9375963db
D3551Z <48575167+D3551Z@users.noreply.github.com>
2020-05-24 02:21:59 +0800

    Add files via upload

---
+++ UniVis/resources/views/components/footer.blade.php
@@ -0,0 +1,13 @@
+<footer>
+    <div class="col">
+		    <h4 class="text-white mb-5">Contacts</h4>
+            <div>
+				<i class="fas fa-phone fa-3x mb-12 text-white"></i>
+				<a>014-6820038 call me maybe</a>
+		    </div>
+			<div>
+				<i class="fas fa-envelope fa-3x mb-12 text-white"></i>
+				<a>yada@yada/com</a>
+			</div>
+     </div>
+</footer>

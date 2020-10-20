abb74cbd931a52ad1af70ce43a663f3c820e3220
D3551Z <48575167+D3551Z@users.noreply.github.com>
2020-10-21 05:12:46 +0800

    New Stuff for Account Page

--- UniVis/resources/views/account_page.blade.php
+++ UniVis/resources/views/account_page.blade.php
@@ -45,6 +45,34 @@
                         <div class="text-center">
                             <button class="btn btn-dark">Update</button>
                         </div>
+                        <!-- Fill in here -->
+                        <div class="file-upload-wrapper">
+                            <label>Your Photo</label>
+                            <input type="file" id="input-file-now" class="file-upload" />
+                        </div>
+                        <div class="md-form">
+                            <i class="fas fa-user prefix grey-text"></i>
+                            <input type="text" id="form-name" class="form-control">
+                            <label for="form-name">Your name</label>
+                        </div>
+                        <div class="md-form">
+                            <i class="fas fa-envelope prefix grey-text"></i>
+                            <input type="text" id="form-email" class="form-control">
+                            <label for="form-email">Your email</label>
+                        </div>
+                        <div class="md-form">
+                            <i class="fas fa-phone prefix grey-text"></i>
+                            <input type="text" id="form-Subject" class="form-control">
+                            <label for="form-Subject">Your contact number</label>
+                        </div>
+                        <div class="md-form">
+                            <i class="fas fa-pencil-alt prefix grey-text"></i>
+                            <textarea id="form-text" class="form-control md-textarea" rows="3"></textarea>
+                            <label for="form-text">Your BIO</label>
+                        </div>
+                        <div class="text-center">
+                            <button class="btn btn-dark">Update</button>
+                        </div>
                     </div>
                 </div>
                 <!-- Form with header -->

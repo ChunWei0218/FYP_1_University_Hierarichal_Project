6cd3cd279fca6740467c2dcacec3694f91051fbd
bryanhon98 <48546959+bryanhon98@users.noreply.github.com>
2020-10-22 13:46:08 +0800

    update

--- UniVis/resources/views/university_page.blade.php
+++ UniVis/resources/views/university_page.blade.php
@@ -1,9 +1,43 @@
 @extends('layouts.graph_layout')
 
 @section('content')
-    <!--content-->
-    <!-- Card Wider -->
-    <div class="card card-cascade wider text center">
+<!--content-->
+<div role="navigation" class="navbar navbar-default navbar-static-top">
+
+    <div class="col-sm-12">
+        <ul class="nav navbar-nav">
+            <li>
+                <form role="search" class="navbar-form" id="search">
+                    <div class="form-group">
+                        <input type="text" value="Bryan" placeholder="Search for Movie Title" class="form-control" name="search">
+                    </div>
+                    <button class="btn btn-black" type="submit">Search</button>
+                </form>
+            </li>
+        </ul>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-8">
+        <div class="panel panel-default">
+            <div class="panel-heading">Search Results</div>
+            <table id="results" class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Project</th>
+                        <th>Department</th>
+                        <th>Universities</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<!-- Card Wider -->
+<div class="card card-cascade wider text center">
 
     <!-- Card graph -->
     <div class="view view-cascade overlay" id="viz">
@@ -27,4 +61,4 @@
     <script src="http://d3js.org/d3.v3.min.js" type="text/javascript"></script>
     <script type="text/javascript" src="neo4j-web.min.js"></script>
     <!--content-->
-@endsection
+    @endsection

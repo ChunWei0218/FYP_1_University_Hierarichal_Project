cb052321c8c1ce9a2fe8e0a5f3bbf9de55b9f73d
ChunWei0218 <37130079+ChunWei0218@users.noreply.github.com>
2020-10-22 14:03:11 +0800

    add edit button

--- UniVis/resources/views/layouts/app.blade.php
+++ UniVis/resources/views/layouts/app.blade.php
@@ -60,6 +60,10 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="">{{ __('Edit') }}</a>
+                            </li>
+                            
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
